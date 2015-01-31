2e088f5eddb23bb8cfd89cef61ab4a0351aab306
Falusi Tamás <puccut@gmail.com>
2015-01-31 15:40:02 +0100

    cherry theme: only admins can go to dashboard

--- wp-content/themes/CherryFramework/functions.php
+++ wp-content/themes/CherryFramework/functions.php
@@ -1603,4 +1603,11 @@
 			return $layout_class;
 		}
 	}
+
+add_action('admin_init', 'no_mo_dashboard');
+function no_mo_dashboard() {
+	if (!current_user_can('manage_options') && $_SERVER['DOING_AJAX'] != '/wp-admin/admin-ajax.php') {
+		wp_redirect(home_url()); exit;
+	}
+}
 ?>
